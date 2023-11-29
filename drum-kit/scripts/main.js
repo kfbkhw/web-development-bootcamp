639cf863db3c9f4804671b3822642e1106485f53
Hailey Kim <kfbkhw@gmail.com>
2023-11-29 21:54:11 +0900

    Project finished - added some functions

--- drum-kit/scripts/main.js
+++ drum-kit/scripts/main.js
@@ -1,8 +1,27 @@
+function playSound(drumButton) {
+    const splitWords = getComputedStyle(drumButton).getPropertyValue('background-image').split('/');
+    const mediaName = splitWords[splitWords.length - 1].split('.')[0];
+    const audio = new Audio(`sounds/${mediaName}.mp3`);
+    audio.play();
+}
+
+function buttonAnimation(drumButton) {
+    drumButton.classList.add('pressed');
+    setTimeout(() => {
+        drumButton.classList.remove('pressed');
+    }, 800);
+}
+
+// play sound on button clicks
 for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
     document.querySelectorAll('.drum')[i].addEventListener('click', function () {
-        const splitWords = getComputedStyle(this).getPropertyValue('background-image').split('/');
-        const mediaName = splitWords[splitWords.length - 1].split('.')[0];
-        const audio = new Audio(`sounds/${mediaName}.mp3`);
-        audio.play();
+        playSound(this);
+        buttonAnimation(this);
     });
 }
+
+// play sound on keyboard presses
+document.addEventListener('keydown', function (event) {
+    playSound(document.querySelector(`.${event.key}`));
+    buttonAnimation(document.querySelector(`.${event.key}`));
+});
