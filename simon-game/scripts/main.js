e50e4efe0991408c326b216f7306f59c5e15a309
Hailey Kim <kfbkhw@gmail.com>
2023-12-02 16:18:37 +0900

    make button click event listener and few changes in html and css

--- simon-game/scripts/main.js
+++ simon-game/scripts/main.js
@@ -1,11 +1,25 @@
-const gamePattern = [];
 const buttonColors = ['red', 'blue', 'green', 'yellow'];
+
+const gamePattern = [];
+const userClickedPattern = [];
+
+function playSound(name) {
+    const audio = new Audio(`sounds/${name}.mp3`);
+    audio.play;
+}
 
 function nextSequence() {
     const randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $('#' + randomChosenColor).fadeOut(100).fadeIn(100);
-    const audio = new Audio(`sounds/${randomChosenColor}.mp3`);
-    audio.play;
+    playSound(randomChosenColor);
 }
+
+$('.container').click((event) => {
+    if (event.target.classList.contains('btn')) {
+        const userChosenColor = event.target.id;
+        userClickedPattern.push(userChosenColor);
+        playSound(userChosenColor);
+    }
+})
