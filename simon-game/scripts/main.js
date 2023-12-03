e0227d3293ff8ff2a4e704e3359a873631bd9348
Hailey Kim <kfbkhw@gmail.com>
2023-12-03 14:22:48 +0900

    add animate function and game start

--- simon-game/scripts/main.js
+++ simon-game/scripts/main.js
@@ -3,12 +3,19 @@ const buttonColors = ['red', 'blue', 'green', 'yellow'];
 const gamePattern = [];
 const userClickedPattern = [];
 
-function playSound(name) {
-    const audio = new Audio(`sounds/${name}.mp3`);
-    audio.play;
-}
+let gameStart = false;
+let gameLevel = 0;
+
+$(document).keypress(() => {
+    if (!gameStart) {
+        nextSequence();
+    }
+})
 
 function nextSequence() {
+    gameStart = true;
+    gameLevel += 1;
+    $('#level-title').html('Level ' + gameLevel)
     const randomNumber = Math.floor(Math.random() * 4);
     const randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
@@ -21,5 +28,18 @@ $('.container').click((event) => {
         const userChosenColor = event.target.id;
         userClickedPattern.push(userChosenColor);
         playSound(userChosenColor);
+        animatePress(userChosenColor);
     }
 })
+
+function playSound(name) {
+    const audio = new Audio(`sounds/${name}.mp3`);
+    audio.play;
+}
+
+function animatePress(currentColor) {
+    $('#' + currentColor).addClass('pressed');
+    setTimeout(() => {
+        $('#' + currentColor).removeClass('pressed');
+    }, 100);
+}
