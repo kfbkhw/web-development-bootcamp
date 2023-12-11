a357353d749f729880ccb64afcb6fdfb7d75212b
Hailey Kim <kfbkhw@gmail.com>
2023-12-11 19:28:37 +0900

    learning npm

---
+++ node/npm/index.js
@@ -0,0 +1,6 @@
+// let generateName = require('sillyname');
+
+import generateName from 'sillyName';
+let sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
---
+++ node/npm/superhero.js
@@ -0,0 +1,4 @@
+import superheroes from 'superheroes';
+const myHeroName = superheroes.random();
+
+console.log(`I am ${myHeroName}!`);
